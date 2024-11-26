fa8eef24810e56deb70295614800787de7a6222d
nicojoaquin <nicolas.joaquin@ulusoft.com.ar>
2024-11-26 15:13:10 -0300

    fix send event

--- apps/voice-gateway/src/voice-gateway.controller.ts
+++ apps/voice-gateway/src/voice-gateway.controller.ts
@@ -1,9 +1,6 @@
-import { Controller, Post, Body, Inject } from '@nestjs/common';
-import { EventPattern } from '@nestjs/microservices';
-import {
-  CallDetails,
-  VoiceGateway
-} from 'apps/voice-gateway/src/interfaces/voice-gateway.interface';
+import { Controller, Inject } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { VoiceGateway } from 'apps/voice-gateway/src/interfaces/voice-gateway.interface';
 
 @Controller('voice-gateway')
 export class VoiceGatewayController {
@@ -11,32 +8,32 @@ export class VoiceGatewayController {
     @Inject('VoiceGateway') private readonly voiceGateway: VoiceGateway
   ) {}
 
-  @EventPattern('voice.initialize')
+  @MessagePattern('voice.initialize')
   async initialize() {
-    return await this.voiceGateway.initialize();
+    return this.voiceGateway.initialize();
   }
 
-  @Post('make-call')
-  async makeCall(@Body() params: CallDetails) {
-    const response = await this.voiceGateway.makeCall(params);
-    return response;
-  }
+  // @Post('make-call')
+  // async makeCall(@Body() params: CallDetails) {
+  //   const response = await this.voiceGateway.makeCall(params);
+  //   return response;
+  // }
 
-  @Post('end-call')
-  async endCall(@Body('callId') callId: string) {
-    await this.voiceGateway.endCall(callId);
-    return { message: 'Call ended successfully' };
-  }
+  // @Post('end-call')
+  // async endCall(@Body('callId') callId: string) {
+  //   await this.voiceGateway.endCall(callId);
+  //   return { message: 'Call ended successfully' };
+  // }
 
-  @Post('get-call-status')
-  async getCallStatus(@Body('callId') callId: string) {
-    const status = await this.voiceGateway.getCallStatus(callId);
-    return status;
-  }
+  // @Post('get-call-status')
+  // async getCallStatus(@Body('callId') callId: string) {
+  //   const status = await this.voiceGateway.getCallStatus(callId);
+  //   return status;
+  // }
 
-  @Post('send-assistant-config')
-  async sendAssistantConfig(@Body('configData') configData: string) {
-    await this.voiceGateway.sendAssistantConfig(configData);
-    return { message: 'Assistant configuration sent successfully' };
-  }
+  // @Post('send-assistant-config')
+  // async sendAssistantConfig(@Body('configData') configData: string) {
+  //   await this.voiceGateway.sendAssistantConfig(configData);
+  //   return { message: 'Assistant configuration sent successfully' };
+  // }
 }
